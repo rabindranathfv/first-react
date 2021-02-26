1e27c345a846ac267d043b04e11d70b7d01a9b16
Rabindranath Ferreira <ferreiralox@gmail.com>
2021-02-26 02:29:55 -0300

    separate into severals components

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
+const ImgLogo = () => <img src={logo} className="App-logo" alt="logo" />;;
+
 function Title(props) {
   return <p>HOLA {props.name} in React, Edit <code>src/App.js</code> and save to reload.</p>;
 }
+
+const message = 'Learn ReactJs power by Facebook';
+class Link extends Component {
+  render() {
+    return (
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        { this.props.msg }
+      </a>
+    );
+  };
+}
+
 class App extends Component {
   render() {
       return (
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            <ImgLogo />
             <Title name='Rabin' />
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
+            <Link msg={message} />
           </header>
         </div>
       );
