d2da794f5f08a8748e130d0b9d3fc12e38d247f9
Rabindranath Ferreira <ferreiralox@gmail.com>
2021-02-26 02:17:02 -0300

    creeate first react component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HOLA REACT
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Title(props) {
+  return <p>HOLA {props.name} in React, Edit <code>src/App.js</code> and save to reload.</p>;
+}
+class App extends Component {
+  render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Title name='Rabin' />
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      );
+  };
 }
 
 export default App;
